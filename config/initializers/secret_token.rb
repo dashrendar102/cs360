d565a8d5bec159d21090e42d14ee93eda4c67385
sgubler <samueljgubler@gmail.com>
2013-11-18 15:47:21 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cs360::Application.config.secret_key_base = '019e729db149e7e1cffe192eac5b65fbb3ae6f19e5d63c7f5ae645073fa0fbb30ae1108bb289ebba207315175c75d455e3f1ff3d3709916b8575180d2ebe2902'
